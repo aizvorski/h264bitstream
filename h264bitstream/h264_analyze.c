44df8fbeef56f278ed33931ba77e05102d8a2035
aizvorski <aizvorski@bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4>
2006-09-27 18:50:47 +0000

    converted spaces to tabs; minor changes in output
    git-svn-id: http://svn.code.sf.net/p/h264bitstream/code/trunk@8 bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4

--- h264bitstream/h264_analyze.c
+++ h264bitstream/h264_analyze.c
@@ -34,53 +34,48 @@
 
 int main(int argc, char *argv[])
 {
-	uint8_t* buf = (uint8_t*)malloc(BUFSIZE);
-	h264_stream_t* h = (h264_stream_t*)malloc(sizeof(h264_stream_t));
-	nal_t* nal = (nal_t*)malloc(sizeof(nal_t));
-	sps_t* sps = (sps_t*)malloc(sizeof(sps_t));
-	pps_t* pps = (pps_t*)malloc(sizeof(pps_t));
-	slice_header_t* sh = (slice_header_t*)malloc(sizeof(slice_header_t));
-	h->nal = nal;
-	h->sps = sps;
-	h->pps = pps;
-	h->sh = sh;
+    uint8_t* buf = (uint8_t*)malloc(BUFSIZE);
+    h264_stream_t* h = h264_new();
 
-	if (argc < 2)
-	{
-		printf("h264_analyze, version 0.1.3\n");
-		printf("Usage: \n");
-		printf("  h264_analyze stream.h264\n");
-		printf("where stream.h264 is a raw H264 stream, as produced by JM or x264\n");
-	}
+    if (argc < 2)
+    {
+        printf("h264_analyze, version 0.1.6\n");
+        printf("Usage: \n");
+        printf("  h264_analyze stream.h264\n");
+        printf("where stream.h264 is a raw H264 stream, as produced by JM or x264\n");
+    }
 
-	int fd = open(argv[1], O_RDONLY);
-	if (fd == -1) { perror("could not open file"); exit(0); }
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) { perror("could not open file"); exit(0); }
 
-	int rsz = 0;
-	int sz = 0;
-	int off = 0;
-	uint8_t* p = buf;
+    int rsz = 0;
+    int sz = 0;
+    int off = 0;
+    uint8_t* p = buf;
 
-	int nal_start, nal_end;
-		
-	while (rsz = read(fd, buf + sz, BUFSIZE - sz))
-	{
-		sz += rsz;
+    int nal_start, nal_end;
+        
+    while ((rsz = read(fd, buf + sz, BUFSIZE - sz)))
+    {
+        sz += rsz;
 
-		while (find_nal_unit(p, sz, &nal_start, &nal_end) > 0)
-		{
-			printf("!! Found NAL at offset %04X, size %04X (%d) \n", 
-				   off + (p - buf) + nal_start, (nal_end - nal_start), (nal_end - nal_start));
-			p += nal_start;
-			read_nal_unit(h, p, nal_end - nal_start);
-			debug_nal(h, h->nal);
-			p += (nal_end - nal_start);
-			sz -= nal_end;
-		}
+        while (find_nal_unit(p, sz, &nal_start, &nal_end) > 0)
+        {
+            printf("!! Found NAL at offset 0x%04X (%d), size 0x%04X (%d) \n", 
+                   off + (p - buf) + nal_start, off + (p - buf) + nal_start, 
+                   (nal_end - nal_start), (nal_end - nal_start));
+            p += nal_start;
+            read_nal_unit(h, p, nal_end - nal_start);
+            debug_nal(h, h->nal);
+            p += (nal_end - nal_start);
+            sz -= nal_end;
+        }
 
-		memmove(buf, p, sz);
-		off += p - buf;
-		p = buf;
-	}
+        memmove(buf, p, sz);
+        off += p - buf;
+        p = buf;
+    }
 
+    h264_free(h);
+    free(buf);
 }
