f6d192a2d44c39c79c78d4f596fe60a136e6e32d
aizvorski <aizvorski@bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4>
2012-02-11 21:11:41 +0000

    bs.h changes: modified reads/writes to track length attempted to be read/written beyond end of existing buffer
    git-svn-id: http://svn.code.sf.net/p/h264bitstream/code/trunk@42 bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4

--- h264bitstream/bs.h
+++ h264bitstream/bs.h
@@ -53,7 +53,8 @@ static void bs_free(bs_t* b);
 static bs_t* bs_clone( bs_t* dest, const bs_t* src );
 static bs_t*  bs_init(bs_t* b, uint8_t* buf, size_t size);
 static uint32_t bs_byte_aligned(bs_t* b);
-static uint32_t bs_eof(bs_t* b);
+static int bs_eof(bs_t* b);
+static int bs_overrun(bs_t* b);
 static int bs_pos(bs_t* b);
 
 static uint32_t bs_peek_u1(bs_t* b);
@@ -74,7 +75,7 @@ static void bs_write_se(bs_t* b, int32_t v);
 static int bs_read_bytes(bs_t* b, uint8_t* buf, int len);
 static int bs_write_bytes(bs_t* b, uint8_t* buf, int len);
 static int bs_skip_bytes(bs_t* b, int len);
-static uint32_t bs_next_bits( bs_t* bs, int nbits );
+static uint32_t bs_next_bits(bs_t* b, int nbits);
 // IMPLEMENTATION
 
 static inline bs_t* bs_init(bs_t* b, uint8_t* buf, size_t size)
@@ -84,20 +85,6 @@ static inline bs_t* bs_init(bs_t* b, uint8_t* buf, size_t size)
     b->end = buf + size;
     b->bits_left = 8;
     return b;
-}
-
-static inline bs_t* bs_clone( bs_t* dest, const bs_t* src )
-{
-    dest->start = src->p;
-    dest->p = src->p;
-    dest->end = src->end;
-    dest->bits_left = src->bits_left;
-    return dest;
-}
-
-static inline uint32_t bs_byte_aligned(bs_t* b) 
-{ 
-    return (b->bits_left == 8);
 }
 
 static inline bs_t* bs_new(uint8_t* buf, size_t size)
@@ -112,7 +99,23 @@ static inline void bs_free(bs_t* b)
     free(b);
 }
 
-static inline uint32_t bs_eof(bs_t* b) { if (b->p >= b->end) { return 1; } else { return 0; } }
+static inline bs_t* bs_clone(bs_t* dest, const bs_t* src)
+{
+    dest->start = src->p;
+    dest->p = src->p;
+    dest->end = src->end;
+    dest->bits_left = src->bits_left;
+    return dest;
+}
+
+static inline uint32_t bs_byte_aligned(bs_t* b) 
+{ 
+    return (b->bits_left == 8);
+}
+
+static inline int bs_eof(bs_t* b) { if (b->p >= b->end) { return 1; } else { return 0; } }
+
+static inline int bs_overrun(bs_t* b) { if (b->p > b->end) { return 1; } else { return 0; } }
 
 static inline int bs_pos(bs_t* b) { return (b->p - b->start); }
 
@@ -121,10 +124,13 @@ static inline int bs_bytes_left(bs_t* b) { return (b->end - b->p); }
 static inline uint32_t bs_read_u1(bs_t* b)
 {
     uint32_t r = 0;
-    if (bs_eof(b)) { return 0; }
     
     b->bits_left--;
-    r = ((*(b->p)) >> b->bits_left) & 0x01;
+
+    if (! bs_eof(b))
+    {
+        r = ((*(b->p)) >> b->bits_left) & 0x01;
+    }
 
     if (b->bits_left == 0) { b->p ++; b->bits_left = 8; }
 
@@ -140,10 +146,11 @@ static inline void bs_skip_u1(bs_t* b)
 static inline uint32_t bs_peek_u1(bs_t* b)
 {
     uint32_t r = 0;
-    if (bs_eof(b)) { return 0; }
-    
-    r = ((*(b->p)) >> ( b->bits_left - 1 )) & 0x01;
 
+    if (! bs_eof(b))
+    {
+        r = ((*(b->p)) >> ( b->bits_left - 1 )) & 0x01;
+    }
     return r;
 }
 
@@ -162,7 +169,10 @@ static inline uint32_t bs_read_u(bs_t* b, int n)
 static inline void bs_skip_u(bs_t* b, int n)
 {
     int i;
-    for ( i = 0; i < n; i++ ) bs_skip_u1( b );
+    for ( i = 0; i < n; i++ ) 
+    {
+        bs_skip_u1( b );
+    }
 }
 
 static inline uint32_t bs_read_f(bs_t* b, int n) { return bs_read_u(b, n); }
@@ -211,11 +221,16 @@ static inline int32_t bs_read_se(bs_t* b)
 
 static inline void bs_write_u1(bs_t* b, uint32_t v)
 {
-    if (bs_eof(b)) { return; }
-    
     b->bits_left--;
-    (*(b->p)) &= ~(0x01 << b->bits_left); // FIXME this is slow, but we must clear bit first - is it better to memset(0) the whole buffer during bs_init() instead?  if we don't do either, we introduce pretty nasty bugs
-    (*(b->p)) |= ((v & 0x01) << b->bits_left);
+
+    if (! bs_eof(b))
+    {
+        // FIXME this is slow, but we must clear bit first
+        // is it better to memset(0) the whole buffer during bs_init() instead? 
+        // if we don't do either, we introduce pretty nasty bugs
+        (*(b->p)) &= ~(0x01 << b->bits_left);
+        (*(b->p)) |= ((v & 0x01) << b->bits_left);
+    }
 
     if (b->bits_left == 0) { b->p ++; b->bits_left = 8; }
 }
@@ -315,52 +330,52 @@ static inline void bs_write_se(bs_t* b, int32_t v)
 
 static inline int bs_read_bytes(bs_t* b, uint8_t* buf, int len)
 {
-    int actual_len;
-    actual_len = len;
+    int actual_len = len;
     if (b->end - b->p < actual_len) { actual_len = b->end - b->p; }
     if (actual_len < 0) { actual_len = 0; }
     memcpy(buf, b->p, actual_len);
-    b->p += actual_len;
+    if (len < 0) { len = 0; }
+    b->p += len;
     return actual_len;
 }
 
 static inline int bs_write_bytes(bs_t* b, uint8_t* buf, int len)
 {
-    int actual_len;
-    actual_len = len;
+    int actual_len = len;
     if (b->end - b->p < actual_len) { actual_len = b->end - b->p; }
     if (actual_len < 0) { actual_len = 0; }
     memcpy(b->p, buf, actual_len);
-    b->p += actual_len;
+    if (len < 0) { len = 0; }
+    b->p += len;
     return actual_len;
 }
 
 static inline int bs_skip_bytes(bs_t* b, int len)
 {
-    int actual_len;
-    actual_len = len;
+    int actual_len = len;
     if (b->end - b->p < actual_len) { actual_len = b->end - b->p; }
     if (actual_len < 0) { actual_len = 0; }
-    b->p += actual_len;
+    if (len < 0) { len = 0; }
+    b->p += len;
     return actual_len;
 }
 
-static inline uint32_t bs_next_bits( bs_t* bs, int nbits )
+static inline uint32_t bs_next_bits(bs_t* bs, int nbits)
 {
    bs_t b;
    bs_clone(&b,bs);
-   return bs_read_u( &b, nbits );
+   return bs_read_u(&b, nbits);
 }
 
-static inline uint64_t bs_next_bytes( const bs_t* const bs, int nbytes )
+static inline uint64_t bs_next_bytes(bs_t* bs, int nbytes)
 {
    int i = 0;
    uint64_t val = 0;
 
-   if ( (nbytes > 8) || (nbytes < 1) ) return 0;
-   if (bs->p + nbytes > bs->end) return 0;
+   if ( (nbytes > 8) || (nbytes < 1) ) { return 0; }
+   if (bs->p + nbytes > bs->end) { return 0; }
 
-   for ( i = 0; i < nbytes; i++ ) val = ( val << 8 ) | bs->p[i];  
+   for ( i = 0; i < nbytes; i++ ) { val = ( val << 8 ) | bs->p[i]; }
    return val;
 }
 
