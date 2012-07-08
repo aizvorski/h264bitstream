cf60a4b38246a71a311be03ab649d46d3c1afe7b
aizvorski <aizvorski@bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4>
2012-07-08 01:29:16 +0000

    macro processor: handle indents/multiline results
    git-svn-id: http://svn.code.sf.net/p/h264bitstream/code/trunk@76 bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4

--- h264bitstream/process.pl
+++ h264bitstream/process.pl
@@ -13,10 +13,10 @@ sub proc_value
     my $code;
     if ($values =~ m{u\((\d+)\)}) { $code = "$s = bs_read_u(b, $1);"; }
     elsif ($values =~ m{u8}) { $code = "$s = bs_read_u8(b);"; }
-    elsif ($values =~ m{(ue|se|ce)}) { $code = "$s = bs_read_$1(b);"; }
+    elsif ($values =~ m{(ue|se|ce|te|me)}) { $code = "$s = bs_read_$1(b);"; }
 
     if ($values eq 'ae') { $code = "$s = bs_read_ae(b);"; }
-    elsif ($values =~ m{ae}) { $code = "if (cabac) { $s = bs_read_ae(b); } else { $code }"; }
+    elsif ($values =~ m{ae}) { $code = "if (cabac) { $s = bs_read_ae(b); }" . "\n${indent}" . "else { $code }"; }
 
-    return $code;
+    return $indent . $code;
 }
