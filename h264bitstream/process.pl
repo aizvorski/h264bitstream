cc414ab3d714a430d99c4251bdbd06924e77a8f2
aizvorski <aizvorski@bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4>
2012-07-08 07:19:13 +0000

    added read_debug macro expansion
    git-svn-id: http://svn.code.sf.net/p/h264bitstream/code/trunk@86 bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4

--- h264bitstream/process.pl
+++ h264bitstream/process.pl
@@ -29,6 +29,13 @@ $code_write =~ s{is_reading}{0}g;
 $code_write =~ s{is_writing}{1}g;
 print $code_write;
 
+$code_read_debug = $code;
+$code_read_debug =~ s{^(\s*) value \s* \( \s* ([^,]*) , (.*) \);}{ &proc_value_read_debug($2, $3, $1) }exmg;
+$code_read_debug =~ s{structure\( (\w+) \)}{read_debug_$1}xg;
+$code_read_debug =~ s{is_reading}{1}g;
+$code_read_debug =~ s{is_writing}{0}g;
+print $code_read_debug;
+
 sub proc_value_read
 {
     my ($s, $values, $indent) = @_;
@@ -46,6 +53,31 @@ sub proc_value_read
     {
         $code = "if (cabac) { $s = bs_read_ae(b); }" . "\n${indent}" . "else { $code }";
     }
+
+    return $indent . $code;
+}
+
+sub proc_value_read_debug
+{
+    my ($s, $values, $indent) = @_;
+    $values =~ s{^\s*}{};
+    $values =~ s{\s*$}{};
+
+    my $code;
+    if ($values =~ m{u\((.*)\)}) { $code = "$s = bs_read_u(b, $1);"; }
+    elsif ($values =~ m{f\((\d+),\s*(.*)\)}) { $code = "/* $s */ bs_skip_u(b, $1);"; }
+    elsif ($values =~ m{(ue|se|ce|te|me|u8|u1)}) { $code = "$s = bs_read_$1(b);"; }
+    elsif ($values eq 'ae') { $code = "$s = bs_read_ae(b);"; }
+    else { $code = "// ERROR: value( $s, $values );"; }
+
+    if ($values =~ m{ae} && $values ne 'ae')
+    {
+        $code = "if (cabac) { $s = bs_read_ae(b); }" . "\n${indent}" . "else { $code }";
+    }
+
+    $code = "printf(\"\%d.\%d: \", b->p - b->start, b->bits_left); ".
+        $code .
+        " printf(\"$s: \%d \\n\", $s); ";
 
     return $indent . $code;
 }
