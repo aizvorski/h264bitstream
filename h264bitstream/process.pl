3fcfaa5b968243b192039b7b698988944ae25976
aizvorski <aizvorski@bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4>
2012-07-08 05:19:55 +0000

    generate read and write code both
    git-svn-id: http://svn.code.sf.net/p/h264bitstream/code/trunk@83 bc2f0a9b-661e-0410-ac89-ff65b7b8b7c4

--- h264bitstream/process.pl
+++ h264bitstream/process.pl
@@ -1,18 +1,24 @@
 undef $/;
-$text = <>;
+$code = <>;
 $/ = "\n";
 
 $decl = '';
-while ($text =~ m{\n(void structure.*)}mg)
+while ($code =~ m{\n(void structure.*)}mg)
 {
     $decl .= $1 . ";\n";
 }
 
-$text =~ s{#function_declarations}{$decl};
+$code =~ s{#function_declarations}{$decl};
 
-$text =~ s{^(\s*) value \s* \( \s* ([^,]*) , (.*) \);}{ &proc_value_read($2, $3, $1) }exmg;
-$text =~ s{structure\( (\w+) \)}{read_$1}xg;
-print $text;
+$code_read = $code;
+$code_read =~ s{^(\s*) value \s* \( \s* ([^,]*) , (.*) \);}{ &proc_value_read($2, $3, $1) }exmg;
+$code_read =~ s{structure\( (\w+) \)}{read_$1}xg;
+print $code_read;
+
+$code_write = $code;
+$code_write =~ s{^(\s*) value \s* \( \s* ([^,]*) , (.*) \);}{ &proc_value_write($2, $3, $1) }exmg;
+$code_write =~ s{structure\( (\w+) \)}{write_$1}xg;
+print $code_write;
 
 sub proc_value_read
 {
@@ -21,14 +27,36 @@ sub proc_value_read
     $values =~ s{\s*$}{};
 
     my $code;
-    if ($values =~ m{u\((\d+)\)}) { $code = "$s = bs_read_u(b, $1);"; }
-    elsif ($values =~ m{(ue|se|ce|te|me|u8)}) { $code = "$s = bs_read_$1(b);"; }
+    if ($values =~ m{u\((.*)\)}) { $code = "$s = bs_read_u(b, $1);"; }
+    elsif ($values =~ m{f\((\d+),\s*(.*)\)}) { $code = "/* $s */ bs_skip_u(b, $1);"; }
+    elsif ($values =~ m{(ue|se|ce|te|me|u8|u1)}) { $code = "$s = bs_read_$1(b);"; }
     elsif ($values eq 'ae') { $code = "$s = bs_read_ae(b);"; }
     else { $code = "// ERROR: value( $s, $values );"; }
 
     if ($values =~ m{ae} && $values ne 'ae')
     {
         $code = "if (cabac) { $s = bs_read_ae(b); }" . "\n${indent}" . "else { $code }";
+    }
+
+    return $indent . $code;
+}
+
+sub proc_value_write
+{
+    my ($s, $values, $indent) = @_;
+    $values =~ s{^\s*}{};
+    $values =~ s{\s*$}{};
+
+    my $code;
+    if ($values =~ m{u\((.*)\)}) { $code = "bs_write_u(b, $1, $s);"; }
+    elsif ($values =~ m{f\((\d+),\s*(.*)\)}) { $code = "/* $s */ bs_write_u(b, $1, $2);"; }
+    elsif ($values =~ m{(ue|se|ce|te|me|u8|u1)}) { $code = "bs_write_$1(b, $s);"; }
+    elsif ($values eq 'ae') { $code = "bs_write_ae(b, $s);"; }
+    else { $code = "// ERROR: value( $s, $values );"; }
+
+    if ($values =~ m{ae} && $values ne 'ae')
+    {
+        $code = "if (cabac) { bs_write_ae(b, $s); }" . "\n${indent}" . "else { $code }";
     }
 
     return $indent . $code;
